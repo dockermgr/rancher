b572db58658b420a7c7158ae13f530d5f7162523
casjay <casjay@yahoo.com>
2021-10-26 20:53:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ REPORAW="$REPO/raw/$REPO_BRANCH"
 APPVERSION="$(__appversion "$REPORAW/version.txt")"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Setup plugins
-HUB_URL="rancher/rancher"
+HUB_URL="rancher/rancher:latest"
 NGINX_HTTP="${NGINX_HTTP:-80}"
 NGINX_HTTPS="${NGINX_HTTPS:-443}"
 SERVER_IP="${CURRIP4:-127.0.0.1}"
